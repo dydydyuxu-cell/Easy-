04aeec65732aa51aa7f2adc217ce75f75e94ebd8
dydydyuxu-cell <dydydyuxu@gmail.com>
2026-09-20 17:12:06 +0300

    Update worker.js

--- cloudflare-worker/worker.js
+++ cloudflare-worker/worker.js
@@ -1,1 +1,12 @@
 
+export default {
+  async fetch(request) {
+    return new Response("Easy API Worker is running", {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+  },
+};
